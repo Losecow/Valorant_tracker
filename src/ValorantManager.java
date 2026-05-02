325fe6689c2f631060508cb0d1af5c93e874f7f8
losecow <jinwoo0712@naver.com>
2026-05-02 20:22:24 +0900

    feat: add fields and constructor to ValorantManager

--- src/ValorantManager.java
+++ src/ValorantManager.java
@@ -1,5 +1,19 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ValorantManager implements ICRUD {
- 
+    
+    // 게임 플레이 기록 목록
+    private ArrayList<Valorant> list = new ArrayList<>();
+    private Scanner sc;
+    private final String FILE_NAME = "valorant_data.txt";
+
+    // 생성자
+    public ValorantManager(Scanner sc) {
+        this.sc = sc;
+        loadFile(); // 파일 불러오기
+    }
+    
 }
