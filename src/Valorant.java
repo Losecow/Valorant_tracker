54d96e7fff3bb366ed1fca8bb49d7db4ca60c8e5
losecow <jinwoo0712@naver.com>
2026-05-02 20:09:01 +0900

    feat: add fields and default constructor to Valorant class

---
+++ src/Valorant.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Valorant {
+    private String name;
+    private String map;
+    private int kill;
+    private int death;
+    private int assist;
+    private int headshot;
+    private int totalDamage;
+    private int headshotPercentage;
+    private String gameDate;
+
+    public Valorant() {
+    }
+
+}
